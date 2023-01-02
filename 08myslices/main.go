bf66c41c5c733110f4ca7098f0f8616c9ef0d762
Kartikeya Chauhan <kartikeyasingh76@gmail.com>
2023-01-02 23:21:09 +0530

    Maps in GoLang

--- 08myslices/main.go
+++ 08myslices/main.go
@@ -7,7 +7,7 @@ func main() {
 
 	// Same as array but without the pre defined length
 	var fruitList = []string{"Apple", "Tomato", "Peach"}
-	fmt.Printf("tyope of fruitlist is %T\n", fruitList)
+	fmt.Printf("type of fruitlist is %T\n", fruitList)
 
 	fruitList = append(fruitList, "mango", "banana")
 
@@ -25,4 +25,16 @@ func main() {
 	highScores[3] = 245
 
 	fmt.Println(highScores)
+
+	// Remove a value from a slice based on the index
+
+	var lists = []string{"1", "two", "3", "four", "5"}
+
+	fmt.Println(lists)
+
+	var index int = 2
+	// append can be used to remove value as well
+	lists = append(lists[:index], lists[index+1:]...)
+	fmt.Println(lists)
+
 }
