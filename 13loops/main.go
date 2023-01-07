abb6e9009275693b4e00ee96252dcf72c31267f0
Kartikeya Chauhan <kartikeyasingh76@gmail.com>
2023-01-07 22:49:57 +0530

    goto statements

--- 13loops/main.go
+++ 13loops/main.go
@@ -9,7 +9,35 @@ func main() {
 
 	fmt.Println(days)
 
-	for d := 0; d < len(days); d++ {
-		fmt.Println(days[d])
+	// for d := 0; d < len(days); d++ {
+	// 	fmt.Println(days[d])
+	// }
+
+	// for i := range days {
+	// 	fmt.Println(days[i])
+	// }
+
+	// for index, day := range days {
+	// 	fmt.Printf("index is %v and value is %v\n", index, day)
+	// }
+
+	rValue := 1
+
+	for rValue < 10 {
+
+		if rValue == 2 {
+			goto plchldr
+		}
+
+		if rValue == 5 {
+			rValue++
+			continue
+		}
+		fmt.Println("Value is: ", rValue)
+		rValue++
 	}
+
+plchldr:
+	fmt.Println("Jumping to placehodler")
+
 }
